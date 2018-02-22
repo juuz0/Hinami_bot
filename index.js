0bf36fa84f793299fab11e22ed54aaf42bdf7735
Nikhil Tanwar <2002nikhiltanwar@gmail.com>
2018-02-22 11:53:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ client.on('ready', () => {
      
      }
 	 if(cmd==='dict'){
-		 webdict('urbandictionary',msg.content.slice(1)).then(response=>{msg.channel.send(response.definition[0])});
+		 webdict('urbandictionary',msg.content.slice(1)).then(response=>{console.log(response)});
 		 }
 	 if(cmd==='invite'){
 		 let embed = new Discord.RichEmbed().setTitle("Touka's Invite Link").setDescription("Feel free to uncheck some permissions").addField("Link :-","https://discordapp.com/oauth2/authorize?&client_id=397248599290806272&scope=bot&permissions=339799126");
