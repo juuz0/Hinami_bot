691530cfda98155b38096f0af0b3bc6b4cae99b9
Nikhil Tanwar <2002nikhiltanwar@gmail.com>
2018-03-30 12:19:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ client.on('ready', () => {
 		 let embed = new Discord.RichEmbed().setTitle("Touka's Invite Link").setDescription("Feel free to uncheck some permissions").addField("Link :-","https://discordapp.com/oauth2/authorize?&client_id=397248599290806272&scope=bot&permissions=339799126");
 		 msg.channel.send({embed:embed});
 		 }
-	 if(cmd=='fd'){fd.push(msg.content.splice(1));
+	 if(cmd=='fd'){fd.push(msg.content.split(" ").splice(1).join(" "));
 				     msg.channel.send("Thank you for your precious feedback")}
 	 if(cmd=='rps'){
 		let choice = ["rock","paper","scissors"];
