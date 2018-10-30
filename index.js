4293f30fb24d617b41a49e2c99dc21d1d0f93173
Nikhil <26814569+2002nikhiltanwar@users.noreply.github.com>
2018-10-30 21:22:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -84,13 +84,15 @@ b.send('Happy Birthday <@' + i.toString() + '>')
     if(cmd === 'mult') {msg.channel.send("Answer is "+(parseFloat(args[0]) * parseFloat(args[1])));}
     if(cmd === 'div') { msg.channel.send("Answer is "+(parseFloat(args[0]) / parseFloat(args[1])));}
     if(cmd === 'sub') { msg.channel.send("Answer is "+(parseFloat(args[0]) - parseFloat(args[1])));}
-    if(cmd=='test'){
    var role = msg.mentions.roles.first();
 	let mmb = msg.mentions.members.first();
 	   mmb.addRole(role);
+	    msg.channel.send(`Added ${role.name}`);
 	   setTimeout(function(){
-		   mmb.removeRole(role);},3000);
-   }
+		   mmb.removeRole(role);
+		   msg.channel.send(`Removed ${role.name}`);
+	   },3000);
+    }
    if(cmd==='say'){
      msg.delete();
    	msg.channel.send(""+msg.content.split(" ").splice(1).join(" "));
