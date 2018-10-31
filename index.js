c46d79de2c9f5c33da3f4cf15bed813fa367c9d3
Nikhil <26814569+2002nikhiltanwar@users.noreply.github.com>
2018-10-31 19:35:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,15 +37,7 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`); 
 	client.user.setGame("with Nikhil | h!help")});
  client.on('message', msg => { 
-	 setInterval(function(){
-var today = new Date()
-var b = msg.guild.channels.find(ch=>ch.name=='birthdays');
-if (b){
-for(var i in birthdays){
-if (birthdays[i].getDate() == today.getDate() && birthdays[i].getMonth() == today.getMonth() && birthdays[i].getFullYear() == today.getFullYear()){
-b.send('Happy Birthday <@' + i.toString() + '>')
-}}}
-},1000);
+
 	 var user_id = msg.author.id;
  	const args = msg.content.slice(prefix.length).trim().split(/ +/g);
  	const cmd = args.shift().toLowerCase();
