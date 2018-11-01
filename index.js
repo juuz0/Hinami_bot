e0a43e24298251c44a6ede97526742f3717a8e35
Nikhil <26814569+2002nikhiltanwar@users.noreply.github.com>
2018-11-01 12:22:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,17 +38,17 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`); 
 	client.user.setGame("with Nikhil | h!help")});
  client.on('message', msg => { 
-	setInterval(function(){
-var today = new Date()
-var b = msg.guild.channels.find(ch=>ch.name=='birthdays');
-if (b){
-for(var i in birthdays){
+// 	setInterval(function(){
+// var today = new Date()
+// var b = msg.guild.channels.find(ch=>ch.name=='birthdays');
+// if (b){
+// for(var i in birthdays){
 	
-if (birthdays[i][1].getDate() == today.getDate() && birthdays[i][1].getMonth() == today.getMonth() && birthdays[i][1].getFullYear() == today.getFullYear()){
-	var user =birthdays[i][0];
-	b.send(`Happy Birthday ${user}! Send Cake!`)
-}}}
-},10000);
+// if (birthdays[i][1].getDate() == today.getDate() && birthdays[i][1].getMonth() == today.getMonth() && birthdays[i][1].getFullYear() == today.getFullYear()){
+// 	var user =birthdays[i][0];
+// 	b.send(`Happy Birthday ${user}! Send Cake!`)
+// }}}
+// },10000);
 	 var user_id = msg.author.id;
  	const args = msg.content.slice(prefix.length).trim().split(/ +/g);
  	const cmd = args.shift().toLowerCase();
