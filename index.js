d897557ff49511f57dc4300754244e9976128aa8
Nikhil <26814569+2002nikhiltanwar@users.noreply.github.com>
2019-05-12 10:37:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on('ready', () => {
  		let userlist = msg.mentions.users;
  		if(userlist.size == 0) {
  			if(!args[0]){
- 		msg.channel.send({embed:new Discord.RichEmbed().setTitle(`${msg.author.username}'s Avatar`).setImage(msg.author.avatarURL.slice(0,-4)+"128")});
+ 		msg.channel.send({embed:new Discord.RichEmbed().setTitle(`${msg.author.username}'s Avatar`).setImage(msg.author.avatarURL)});
 														 
  		
  		else if(args[0]){
